aca2c898eefe83e98a5d3d014d80b3b108b3327b
Jacob <soccerjacobjm17@gmail.com>
2021-08-21 12:07:28 -0400

    create login page and update index.js file

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,55 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Button } from 'react-bootstrap'
+import React, {useState} from 'react'
+import { Link, Redirect } from 'react-router-dom'
 
-export default function Login() {
+const Login = ({setAuthenticated}) => {
+    const [formSubmittedSuccessfully, setFormSubmittedSuccessfully] = useState(false);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function authenticate(event) {
+        event.preventDefault();
+        if (username && password) {
+            fetch('https://strangers-things.herokuapp.com/api/2105-VPI-RM-WEB-PT/users/login', {
+                method: "POST", 
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    user: {
+                        username: '{username}',
+                        password: '{password}'
+                    }
+                })
+            }).then(response => response.json())
+            .then(result => {
+                console.log(result);
+            })
+            .catch(console.error);
+        }
+        setFormSubmittedSuccessfully(true);
+    }
+
+    if(formSubmittedSuccessfully) {
+        return <Redirect to="/Signup" />
+    }
+
     return (
-        <>
-        <div>
-            This is where user will log in
-
-        </div>
-
-        <div>
-
-            <div>
-            <input type='text' placeholder='Enter Username' />
-            </div>
-
-            <div>
-            <input type='text' placeholder='Enter Password' />
-            </div>
-
-            <div>
-                <Button><Link to="/home">Log in</Link></Button>
-                <Button><Link to='/home'>Cancel</Link></Button>
-            </div>
-            
-        </div>
-
-        </>
+        <section className="login">
+        <form onSubmit={authenticate}>
+            <label>
+                Username:
+            <input type="text" username="username" onChange= {(event) => setUsername(event.target.value)} value={username}/>
+            </label>
+            <label>
+                Password:
+            <input type="text" password="password" onChange= {(event) => setPassword(event.target.value)} value={password}/>
+            </label>
+            <button type="submit">LOGIN</button>
+            <Link className="lnkBtn" to="/Signup">SIGN UP</Link>
+        </form>
+        </section>
     )
+    
 }
+
+export default Login;
