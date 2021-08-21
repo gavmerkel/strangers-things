0a8d3ca9bb01dbdc40fd97b13724a65f4328b742
Frosty <Farhan.konsulting@gmail.com>
2021-08-21 12:13:49 -0400

    adding headers

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
 
 export function EmptyHeader() {
     return (
@@ -20,6 +21,8 @@ export function UnauthenticatedHeader() {
             <div className="square border-bottom border-dark border-3">
                 {/* <div className="ms-5 fs-3 text-start fw-bold"> Stranger's Things UNAUTH</div> */}
                 <Link to='/home' className="ms-5 fs-3 text-start fw-bold text-decoration-none text-reset">Stranger's Things UNAUTH</Link>
+
+                <Button></Button>
             </div>
         </header>
     )
